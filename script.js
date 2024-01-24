70973d641aeedfe7b6e94190a0b0713a2c0cb1cb
Guttenberg Junior <ghuttojunior@gmail.com>
2024-01-23 23:44:58 -0300

    third commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let burger = document.getElementById('burger'),
 	 nav    = document.getElementById('main-nav');
+	 navRedirect = document.querySelectorAll(".nav_redirect");
 
 burger.addEventListener('click', function(e){
 	this.classList.toggle('is-open');
 	nav.classList.toggle('is-open');
 });
-
